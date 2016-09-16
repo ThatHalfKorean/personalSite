acb3ae76991aee0408661f4843d183426ecc40e6
Geoff Kim <gkim6@lion.lmu.edu>
2016-09-16 00:11:50 -0700

    CSS and minor refactoring

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -62,29 +62,31 @@ const dealerCompare = {
         [-1,  -1,  'Split', 'Split', 'Split', 'Split', 'Split', 'Split', 'Split', 'Split', 'Split', 'Split']
         ]
 
-}
-const handType = ['Hard', 'Soft', 'Pair']
+};
+const handType = ['Hard', 'Soft', 'Pair'];
 const getRandomIntInRange = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-var situations = [{
-                  'min':5,
-                  'max':21,
-                  'description': 'A Hard ',
-                  'table':'Hard'
-                 },
-                 {
-                  'min':13,
-                  'max':21,
-                  'description': 'A Soft ',
-                  'table':'Soft'
-                 },
-                 {
-                  'min':2,
-                  'max':11,
-                  'description': 'A Pair of ',
-                  'table':'Pair'                  
-                 }];
+};
+const situations = [
+    {
+        'min':5,
+        'max':21,
+        'description': 'A Hard ',
+        'table':'Hard'
+    },
+    {
+        'min':13,
+        'max':21,
+        'description': 'A Soft ',
+        'table':'Soft'
+    },
+    {
+        'min':2,
+        'max':11,
+        'description': 'A Pair of ',
+        'table':'Pair'                  
+    }
+];
 const BlackJackQuiz = React.createClass({
     getInitialState() {
         let startType = situations[Math.floor(Math.random() * 3)]
@@ -109,22 +111,22 @@ const BlackJackQuiz = React.createClass({
         let you = this.state.yourHand
         let strat = dealerCompare[this.state.typeOfHand][you][dealer]
         if(input === strat){
-            this.setState({answer:'truuu'})
+            this.setState({answer:'Correct'})
         } else {
-            this.setState({answer:'nawww'})
+            this.setState({answer:'Try Again'})
         }
     },
     render() {
         return (
-            <div>
-                <div>The dealer is showing a {this.state.dealerHand} and you have a {this.state.typeOfHand} {this.state.typeOfHand === 'Pair'?('of '+this.state.yourHand+'\'s'):this.state.yourHand} you should?</div>
-                <div>Answer: {this.state.answer}</div>
-                <div>
+            <div className="quiz-content">
+                <div className="quiz-prompt">The dealer is showing a {this.state.dealerHand} and you have a {this.state.typeOfHand} {this.state.typeOfHand === 'Pair'?('of '+this.state.yourHand+'\'s'):this.state.yourHand} you should?</div>
+                <div className="quiz-answer-buttons">
                     <button onClick={()=>{this.compareHands('Hit')}}>Hit</button>
                     <button onClick={()=>{this.compareHands('Stay')}}>Stay</button>
                     <button onClick={()=>{this.compareHands('Double')}}>Double</button>
                     <button onClick={()=>{this.compareHands('Split')}}>Split</button>
                 </div>
+                <div className="answer">Answer: <div className="revealed">{this.state.answer}</div></div>
                 <div>
                     <button onClick={()=>{this.resetQuiz()}}>Next</button>
                 </div>
@@ -132,38 +134,40 @@ const BlackJackQuiz = React.createClass({
             </div>
         )
     }
-})
+});
 const BlackJackLookUp = React.createClass({
     getInitialState() {
         return {
-            dealerHand: 5,
-            yourHand: 2,
+            dealerHand: 2,
+            yourHand: 5,
             typeOfHand: 'Hard'
         }
     },
     render(){
-        return(<div>
-            <div>
-                If you have a:
+        return(<div className="lookup-content">
+            <div className="lookup-your-hand">
+                <div>
+                    If you have a:
+                </div>
                 <select onChange={e => this.setState({ yourHand: e.target.value.split('-')[1],  typeOfHand: e.target.value.split('-')[0]})}>
                     <option value="" disabled>Hard</option>
-                    <option value="Hard-5">5</option>
-                    <option value="Hard-6">6</option>
-                    <option value="Hard-7">7</option>
-                    <option value="Hard-8">8</option>
-                    <option value="Hard-9">9</option>
-                    <option value="Hard-10">10</option>
-                    <option value="Hard-11">11</option>
-                    <option value="Hard-12">12</option>
-                    <option value="Hard-13">13</option>
-                    <option value="Hard-14">14</option>
-                    <option value="Hard-15">15</option>
-                    <option value="Hard-16">16</option>
-                    <option value="Hard-17">17</option>
-                    <option value="Hard-18">18</option>
-                    <option value="Hard-19">19</option>
-                    <option value="Hard-20">20</option>
-                    <option value="Hard-21">21</option>
+                    <option value="Hard-5">Hard 5</option>
+                    <option value="Hard-6">Hard 6</option>
+                    <option value="Hard-7">Hard 7</option>
+                    <option value="Hard-8">Hard 8</option>
+                    <option value="Hard-9">Hard 9</option>
+                    <option value="Hard-10">Hard 10</option>
+                    <option value="Hard-11">Hard 11</option>
+                    <option value="Hard-12">Hard 12</option>
+                    <option value="Hard-13">Hard 13</option>
+                    <option value="Hard-14">Hard 14</option>
+                    <option value="Hard-15">Hard 15</option>
+                    <option value="Hard-16">Hard 16</option>
+                    <option value="Hard-17">Hard 17</option>
+                    <option value="Hard-18">Hard 18</option>
+                    <option value="Hard-19">Hard 19</option>
+                    <option value="Hard-20">Hard 20</option>
+                    <option value="Hard-21">Hard 21</option>
                     <option value="" disabled>Soft</option>
                     <option value="Soft-13">Soft 13</option>
                     <option value="Soft-14">Soft 14</option>
@@ -184,8 +188,10 @@ const BlackJackLookUp = React.createClass({
                     <option value="Pair-11">A,A</option>
                 </select>
             </div>
-            <div>
-                and the dealer is showing:
+            <div className="lookup-dealer-hand">
+                <div>
+                    And the dealer is showing:
+                </div>
                     <select onChange={e => this.setState({ dealerHand: e.target.value })}>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -200,12 +206,15 @@ const BlackJackLookUp = React.createClass({
                     </select>
             </div>
             <div>
-                You Should: {dealerCompare[this.state.typeOfHand][this.state.yourHand][this.state.dealerHand]}
+                You Should:
+            </div>
+            <div className="revealed">
+                {dealerCompare[this.state.typeOfHand][this.state.yourHand][this.state.dealerHand]}
             </div>
         </div>)
     }
 
-})
+});
 const App = React.createClass({
     getInitialState() {
         return {
@@ -213,18 +222,19 @@ const App = React.createClass({
         }
     },
     render(){
-        var test = false
         return (
             <div>
-                <div>
-                    <div className={this.state.activeIndex === 0 ? 'active':''} onClick={()=> this.setState({activeIndex: 0})}>Quiz</div>
-                    <div className={this.state.activeIndex === 1 ? 'active':''} onClick={()=> this.setState({activeIndex: 1})}>Look Up</div>
+                <div className='tabs'>
+                    <div className={this.state.activeIndex === 0 ? 'tab active':'tab'} onClick={()=> this.setState({activeIndex: 0})}>Quiz</div>
+                    <div className={this.state.activeIndex === 1 ? 'tab active':'tab'} onClick={()=> this.setState({activeIndex: 1})}>Look Up</div>
                 </div>
-               {(this.state.activeIndex === 0 && <BlackJackQuiz/>)}
-               {(this.state.activeIndex === 1 && <BlackJackLookUp/>)}
+                <div>
+                   {(this.state.activeIndex === 0 && <BlackJackQuiz/>)}
+                   {(this.state.activeIndex === 1 && <BlackJackLookUp/>)}
+               </div>
             </div>
         )
     }
-})
+});
 
 ReactDOM.render(<App/>, document.getElementById('container'))
